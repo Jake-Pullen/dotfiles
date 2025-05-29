0e2e72ded77c6b1d472c3650ff87484b0633fb48
Jake Pullen <88822333+Jake-Pullen@users.noreply.github.com>
2025-05-29 07:47:06 +0100

    chore: 🧹 Cleaning up based on recent events O.O

--- set_up_dev/install_me.sh
+++ set_up_dev/install_me.sh
@@ -3,32 +3,6 @@
 # Get current user
 USER=$(whoami)
 
-# Define paths
-SSH_DIR="/home/$USER/.ssh"
-KEY_FILE="$SSH_DIR/id_rsa.pub"
-
-# Create .ssh directory if it doesn't exist
-if [ ! -d "$SSH_DIR" ]; then
-    mkdir -p "$SSH_DIR"
-    echo "Created .ssh directory for user $USER"
-fi
-
-# Check if the public key file exists
-if [ -f "$KEY_FILE" ]; then
-    echo "Public key already exists at $KEY_FILE"
-else
-    # Generate SSH key with a comment based on user, hostname, and timestamp
-    ssh-keygen -C "$USER@$(uname -n)-$(date -I)" -f "$SSH_DIR/id_rsa"
-    echo "SSH key generated successfully at $KEY_FILE"
-fi
-
-# Optionally, display the public key if it was generated
-if [ -f "$KEY_FILE" ]; then
-    echo "Public key content:"
-    cat "$KEY_FILE"
-fi
-
-
 package_list=(
     "alacritty"
     "diff-so-fancy"
@@ -56,7 +30,7 @@ sudo pacman -S --needed --noconfirm $package_string
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 ## Linutil by CTT
-curl -fsSL https://christitus.com/linux | sh
+# curl -fsSL https://christitus.com/linux | sh
 
 ## make zsh the default shell
 chsh /usr/bin/zsh
