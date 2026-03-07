17a6c5aefd911aaa20d7db475805940430bcf851
Jake Pullen <hello@jake-is.me>
2026-03-07 18:10:25 +0000

    pre-merge commit

--- set_up_dev/restore_ssh_gpg.sh
+++ set_up_dev/restore_ssh_gpg.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# FIRST, MOUNT NAS AT /mnt/nas/ 
 
-REMOTE="devin@192.168.0.49"
+REMOTE="/mnt/nas/Jake
 
 echo "📥 Pulling SSH keys..."
-rsync "$REMOTE:~/.ssh/id_rsa" ~/.ssh/ && chmod 600 ~/.ssh/id_rsa
-rsync "$REMOTE:~/.ssh/id_rsa.pub" ~/.ssh/ && chmod 644 ~/.ssh/id_rsa.pub
-rsync "$REMOTE:~/.ssh/config" ~/.ssh/ && chmod 644 ~/.ssh/config
+rsync "$REMOTE/ssh/id_rsa" ~/.ssh/ && chmod 600 ~/.ssh/id_rsa
+rsync "$REMOTE/ssh/id_rsa.pub" ~/.ssh/ && chmod 644 ~/.ssh/id_rsa.pub
+rsync "$REMOTE/ssh/config" ~/.ssh/ && chmod 644 ~/.ssh/config
 
 echo "📥 Pulling GPG key..."
-rsync "$REMOTE:~/my-gpg-key.asc" ~/
+rsync "$REMOTE/ssh/my-gpg-key.asc" ~/
 
 echo "🔑 Importing GPG key..."
 gpg --import ~/my-gpg-key.asc
