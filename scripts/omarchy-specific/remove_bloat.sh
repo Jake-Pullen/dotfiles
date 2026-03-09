74cf2ed2fcfba04a1a1325fd726823e9df7ff2a5
Jake Pullen <hello@jake-is.me>
2026-03-09 17:12:31 +0000

    little tweak

--- scripts/omarchy-specific/remove_bloat.sh
+++ scripts/omarchy-specific/remove_bloat.sh
@@ -9,6 +9,17 @@ omarchy-webapp-remove "Figma" 2>/dev/null || true
 omarchy-webapp-remove "Zoom" 2>/dev/null || true
 omarchy-webapp-remove "Fizzy" 2>/dev/null || true
 
+
 omarchy-pkg-drop signal-desktop 2>/dev/null || true
+omarchy-pkg-drop 1password-cli 2>/dev/null || true
+omarchy-pkg-drop 1password-beta 2>/dev/null || true
+omarchy-pkg-drop aether 2>/dev/null || true
+omarchy-pkg-drop kdenlive 2>/dev/null || true
+omarchy-pkg-drop libreoffice-fresh 2>/dev/null || true
+omarchy-pkg-drop localsend 2>/dev/null || true
+omarchy-pkg-drop obs-studio 2>/dev/null || true
+omarchy-pkg-drop pinta 2>/dev/null || true
+omarchy-pkg-drop typora 2>/dev/null || true
+omarchy-pkg-drop xournalpp 2>/dev/null || true
 
 rm -rf ~/Work/tries 2>/dev/null || true
